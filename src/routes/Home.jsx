2efc27cf0b53a6f786449cbc1fa21d08e08e8329
Alexandre Oliveira <Alexandre2001.oliveira@gmail.com>
2023-10-23 15:27:59 -0300

    Loading API Data and Canvas View

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -13,10 +13,9 @@ const Home = () => {
     
     try{
       const response = await axios.get("https://jsonplaceholder.typicode.com/posts")
-
       const data = response.data
-      console.log(data)
 
+      setPosts(data);
     }catch(error){
       console.log(error)
     }
@@ -30,7 +29,18 @@ const Home = () => {
   }, [])
 
   return (
-    <div>Home</div>
+    <div>
+      <h1>últimos posts</h1>
+      {posts.length ===0 ? (<p>Carregando...</p>) : (
+        posts.map((post) => (
+          <div className="post" key={post.id}>
+            <h2>{post.title}</h2>
+            <p>{post.body}</p>
+            <Link to={`/posts/${post.id}`} className='btn' >Ler mais</Link>
+          </div>
+        ))
+      )}
+    </div>
   )
 }
 
