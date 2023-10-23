f5b59a165d2d729196b046addad46f2d3c45fa4e
Alexandre Oliveira <Alexandre2001.oliveira@gmail.com>
2023-10-23 15:20:14 -0300

    Application integration with the API

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,9 +1,34 @@
 import React from 'react'
+import "./Home.css";
 import { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
-import "./Home.css"
+import axios from 'axios';
+
 
 const Home = () => {
+
+  const [posts, setPosts] = useState([])
+
+  const getPosts = async() =>{
+    
+    try{
+      const response = await axios.get("https://jsonplaceholder.typicode.com/posts")
+
+      const data = response.data
+      console.log(data)
+
+    }catch(error){
+      console.log(error)
+    }
+
+  }
+
+  useEffect(() =>{
+
+    getPosts();
+
+  }, [])
+
   return (
     <div>Home</div>
   )
